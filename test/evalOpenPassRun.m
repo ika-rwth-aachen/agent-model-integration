2b4339083fdb351d6da2b079a7161ca7ec825c34
DBE <daniel.becker@ika.rwth-aachen.de>
2020-10-09 10:49:16 +0200

    beautify plot

--- test/evalOpenPassRun.m
+++ test/evalOpenPassRun.m
@@ -1,17 +1,18 @@
 run = readmatrix('D:\home\git\SETLevel4to5\openpass\Binaries\Windows\debug.txt');
-figure(1); % x,y plane
+figure(1); 
+subplot(2,2,1);% x,y plane
 plot(run(:,2),run(:,3),'.');
 daspect([1 1 1]);
 title('x,y-plane');
 
-figure(2); % acceleration over time
+subplot(2,2,2); % acceleration over time
 plot(run(:,1),run(:,4));
 title('Acceleration');
 
-figure(3); % speed over time
+subplot(2,2,3); % speed over time
 plot(run(:,1),run(:,5));
 title('Speed');
 
-figure(4); % yaw rate over time 
+subplot(2,2,4); % yaw rate over time 
 plot(run(:,1),run(:,6));
 title('yaw rate');
