65b2bb36aae909539bc9adf50e61484081c1ac60
DBE <daniel.becker@ika.rwth-aachen.de>
2020-10-21 10:17:00 +0200

    update script

--- test/evalOpenPassRun.m
+++ test/evalOpenPassRun.m
@@ -18,25 +18,26 @@ plot(run(:,1),run(:,6));
 title('yaw rate');
 
 figure(2);
+subplot(2,2,1);
 plot(run(:,1),run(:,7));
 title('a desired');
-figure(3);
+subplot(2,2,2);
 plot(run(:,1),run(:,8));
 title('v local');
-figure(4);
+subplot(2,2,3);
 plot(run(:,1),run(:,9));
 title('v pred');
 
 %% horizon
 horizon = readmatrix('D:\home\git\SETLevel4to5\openpass\Binaries\Windows\horizon.txt');
-i=341; % timestamp. 
+i=209; % timestamp. 
 a=i*32 + 1; b=(i+1)*32; 
 
 figure(5);
 subplot(2,1,1);
 plot(horizon(a:b,1),horizon(a:b,2));
-title(['x,y-horizon at t = ',num2str(i),'s']);
+title(['x,y-horizon at t = ',num2str(i/10),'s']);
 daspect([1 1 1]);
 subplot(2,1,2);
-plot(horizon(a:b,3));
+plot(horizon(a:b,3),horizon(a:b,4));
 title('kappa');
