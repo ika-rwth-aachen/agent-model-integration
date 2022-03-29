1d9cd3b9f0ebfe34f8ed0076bc9fbe25f1c1759e
Daniel Becker <daniel.becker@ika.rwth-aachen.de>
2022-03-29 22:28:33 +0200

    clean up and documentation

--- test/osiTraceTest.cpp
+++ test/osiTraceTest.cpp
@@ -9,12 +9,15 @@
 
 std::string getFileBuffer(char* fName)
 {
+    // open file
     std::ifstream osifile(fName, std::ifstream::binary); 
+    // get file length
     osifile.seekg (0, osifile.end);
     int file_size = osifile.tellg();
     osifile.seekg (0, osifile.beg);
 
     std::cout << "Reading: " << fName << " with file_size: " << file_size << "\n";
+    // read complete trace into string byte buffer    
     std::string buffer;
     buffer.resize(file_size);
     osifile.read(&buffer[0],  buffer.size() );    
@@ -23,27 +26,45 @@ std::string getFileBuffer(char* fName)
     return buffer;
 }
 
+/**
+ * @brief Reads the next osi3 message in the remaining Byte buffer
+ * 
+ *  This function checks the first four Bytes for the length of the next osi 
+ *  message. Then it extracts the substring of this message and parses the 
+ *  Bytes in an osi3 message of type <T>. Finally, the parsed message gets
+ *  deleted from the Byte buffer `buf`.
+ * @tparam T an osi3 class which should be extracted from the Byte buffer
+ * @param buf Byte buffer that holds the remaining trace that shall be parsed.
+ * @param osi_msg The message that should be filled. Passed by reference.
+ * @return true If an osi3 message could be extracted.
+ * @return false If either the remaining buffer `buf` is too short, or the
+ *               extraction of the osi3 message failed.
+ */
 template <class T>
 bool nextOsiMsg(std::string &buf, T &osi_msg)
 {
+    // check if remaining buffer is long enough
     unsigned long int msg_size = 0;
     if (buf.size() < sizeof(msg_size)) return false;  
     
+    // extract the length of the next osi3 message. This is done by little
+    // Endian encoding. The first four Bytes depict the message size.
     std::string msg_len = buf.substr(0,4);
     msg_size += (((unsigned long int)((unsigned char)msg_len[0])) << 0);
     msg_size += (((unsigned long int)((unsigned char)msg_len[1])) << 8);
     msg_size += (((unsigned long int)((unsigned char)msg_len[2])) << 16);
     msg_size += (((unsigned long int)((unsigned char)msg_len[3])) << 24);
-    //std::cout << "msg_size: " << msg_size << "\n";
 
     bool parsed = osi_msg.ParseFromString(buf.substr(4,msg_size));
 
+    // Delete the four Bytes that depict the message size and the message itself
     buf.erase(0, msg_size+4);
     return parsed;
 }
 
 int main(int argc, char *argv[]) 
 {
+    // Initialize required osi3 messages and driver model
     osi3::SensorView sv;
     osi3::TrafficCommand tc;
     osi3::TrafficUpdate up;
@@ -51,34 +72,48 @@ int main(int argc, char *argv[])
     setlevel4to5::DynamicsRequest dr;
     IkaAgent test_agent;
     
-    // get complete buffers for sensor view and traffic command
+    // Get complete buffers for sensor view and traffic command
     std::string buffer_sv = getFileBuffer(argv[1]);
     std::string buffer_tc = getFileBuffer(argv[2]);
 
-    // search for first sv and tc
+    // Search for first sv and tc
     bool found_sv = nextOsiMsg(buffer_sv, sv);
     bool found_tc = nextOsiMsg(buffer_tc, tc);
+
+    // Variables for time stamp and delta t
     double dt = 0.01; //first estimate
     double last_t;
-    double t = sv.timestamp().seconds()+ double(sv.timestamp().nanos())/1000000000;
+    double t = double(sv.timestamp().seconds())
+             + double(sv.timestamp().nanos())/1000000000;
+    
+    // Loop over the sensor view buffer as long as a sensor is found
     while (found_sv)
     {
-        //std::cout << "sv time stamp: " << sv.timestamp().seconds()+ double(sv.timestamp().nanos())/1000000000 << std::endl;
+        // Check if new traffic command was found. 
         if (found_tc)
             std::cout << "new traffic command with dest.: " 
-                << tc.action(0).acquire_global_position_action().position().x() << ","
+                << tc.action(0).acquire_global_position_action().position().x()
+                << ","
                 << tc.action(0).acquire_global_position_action().position().y()
                 << "\n";
-        // perform open loop step 
+        // Perform open loop step of the driver model
         test_agent.step(t, dt, sv, tc, up, dr);     
-        std::cout << "new update: " << up.update(0).base().position().x() << "," << up.update(0).base().position().y() << "\n";
-        last_t = t;   
+        std::cout << "new traffic update: " 
+                  << up.update(0).base().position().x() << "," 
+                  << up.update(0).base().position().y() << "\n";
+
+        last_t = t; // shift processed time stamp for delta t calculation
+        
+        // When another sensor view was found, make sure the loop continues
+        // and calculate the delta t between new and old sensor view.
         found_sv = nextOsiMsg(buffer_sv, sv);
         if (found_sv)
         {
-            t = sv.timestamp().seconds()+ double(sv.timestamp().nanos())/1000000000;
+            t = double(sv.timestamp().seconds())
+              + double(sv.timestamp().nanos())/1000000000;
             dt = t - last_t;
         }
+        // When no new traffic command was found, clear the osi3 message
         found_tc = nextOsiMsg(buffer_tc, tc);
         if (!found_tc) tc.Clear();
     }
