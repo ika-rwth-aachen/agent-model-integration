a9a6ed6ddc8e11c2a4d142cb88170624984229cb
Daniel Becker <daniel.becker@ika.rwth-aachen.de>
2022-04-04 16:27:35 +0200

    Tune parameters for new lat. ctrl.

--- src/IkaAgent.cpp
+++ src/IkaAgent.cpp
@@ -64,10 +64,10 @@ void IkaAgent::init(osi3::BaseMoving &host) {
   driver_param->steering.thw[1] = 3.0;
   driver_param->steering.dsMin[0] = 1.0;
   driver_param->steering.dsMin[1] = 10.0;
-  driver_param->steering.P[0] = 0.075 * wheel_base;
-  driver_param->steering.P[1] = 0.03 * wheel_base;
-  driver_param->steering.D[0] = 0.25;
-  driver_param->steering.D[1] = 0.25;
+  driver_param->steering.P[0] = 2.5 * wheel_base;
+  driver_param->steering.P[1] = 0.0 * wheel_base;
+  driver_param->steering.D[0] = 10.0;
+  driver_param->steering.D[1] = 25.00;
 
   AgentModel::init();
 
