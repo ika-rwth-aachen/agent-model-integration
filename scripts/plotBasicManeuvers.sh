a693a396f609c99a9e47011909e98a8545bd6eea
Christian Geller <christian.geller@ika.rwth-aachen.de>
2023-02-27 10:48:16 +0100

    update setlevel plot script

--- scripts/plotBasicManeuvers.sh
+++ scripts/plotBasicManeuvers.sh
@@ -1,5 +1,5 @@
 # commands to get the plots used in the set_level ASE presentation
-python3 plotState.py -f ../debug/vehicle_16.json -p subconscious_a -p2 subconscious_kappa -a -t 23.0-40.0 -s
-python3 plotState.py -f ../debug/vehicle_18.json -p vs_v -p2 subconscious_a -a -t 0.0-13.5 -s
-python3 plotState.py -f ../debug/vehicle_1.json -p ego_d -p2 ego_psi -a -t 7.0-22.0 -s
-python3 plotState.py -f ../debug/vehicle_4.json -p subconscious_a -p2 THW -a -t 0.0-15.0 -s
+python3 plotState.py -f ../debug/vehicle_16_16_2023-02-10_13-43-14.json -p subconscious_a -p2 subconscious_kappa -t 23.0-40.0 -s
+python3 plotState.py -f ../debug/vehicle_18_18_2023-02-10_13-43-14.json -p vs_v -p2 subconscious_a -t 0.0-13.5 -s
+python3 plotState.py -f ../debug/vehicle_1_1_2023-02-15_15-46-37.json -p ego_d -p2 subconscious_kappa -t 7.0-22.0 -s
+python3 plotState.py -f ../debug/vehicle_4_4_2023-02-10_13-43-14.json -p THW -p2 subconscious_a -t 0.0-15.0 -s
